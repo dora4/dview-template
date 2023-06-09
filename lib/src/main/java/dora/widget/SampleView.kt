2205e529d20ac7d4ba213b75cc22422f0a145885
dora4 <71242257+dora4@users.noreply.github.com>
2023-06-10 00:30:57 +0800

    新增模板view代码

---
+++ lib/src/main/java/dora/widget/SampleView.kt
@@ -0,0 +1,9 @@
+package dora.widget
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+class SampleView @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defAttr: Int = 0)
+    : View(context, attrs, defAttr) {
+}
