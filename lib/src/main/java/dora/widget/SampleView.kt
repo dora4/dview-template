f2721b293f17d7e1a7b5d42dd12f6d637f81aaf1
dora4 <71242257+dora4@users.noreply.github.com>
2023-06-24 22:24:52 +0800

    更新模板代码

--- lib/src/main/java/dora/widget/SampleView.kt
+++ lib/src/main/java/dora/widget/SampleView.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.View
 
-class SampleView @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defAttr: Int = 0)
-    : View(context, attrs, defAttr) {
+class SampleView @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0,
+                                           defStyleRes: Int = 0)
+    : View(context, attrs, defStyleAttr, defStyleRes) {
 }
